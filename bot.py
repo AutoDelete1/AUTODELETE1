e99c302146c92fc4e797455f49352442a5a2e489
AutoDelete1 <113407963+AutoDelete1@users.noreply.github.com>
2022-09-13 09:48:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,16 @@ import asyncio
 from os import environ
 from pyrogram import Client, filters, idle
 
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
-BOT_TOKEN = environ.get("BOT_TOKEN")
-SESSION = environ.get("SESSION")
-TIME = int(environ.get("TIME"))
+API_ID = int(environ.get("API_ID", "8192282"))
+API_HASH = environ.get("API_HASH", "990a85e4f02364ddf5927728e75450b5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "2003350455:AAH0J2h2lm6vUeUrSjustbl8Z1jiQi1IyVY")
+SESSION = environ.get("SESSION", "BQCQ-iY7B6PuFhpxxK0u9WnQqdivMLrIEHilv7z01uPqQI5A8zeiFqRciEmk4w0vhFbA23dnWP6lz9Nzo865NzenHi-rjxgIv2djUjhgSy3apmiCEAYVtir6CbG7ppxS0uw2RV1AEwJUxhtDhLGXDeBbEssN5ME16vpVhL5EgM54_fXbQLd_Tm4XivB01wn8WenUKmujfDdW2o-IEdk9hV-r5VtNV8CWjjGegBDouvxVexx71SP-LJ3wzt4ouPnqWOji3Ar-IU1xK8drcntLs5EI7f7cDM2wTQUv0_Tz1-cJgsVxW0HrXFH6grZiUmDr8czq6otAUitKUvlMxPKxTGMzMRtyrQA")
+TIME = int(environ.get("TIME", "10"))
 GROUPS = []
-for grp in environ.get("GROUPS").split():
+for grp in environ.get("GROUPS", "-1001681225968").split():
     GROUPS.append(int(grp))
 ADMINS = []
-for usr in environ.get("ADMINS").split():
+for usr in environ.get("ADMINS", "823882413").split():
     ADMINS.append(int(usr))
 
 START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
